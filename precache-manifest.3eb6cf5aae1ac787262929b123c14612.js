4d5a43370a92f2fb1a4b8f06946aab4355c967d5
kelbek <dev_kelbek@mail.ru>
2020-05-27 17:45:43 +0000

    Updates

---
+++ precache-manifest.3eb6cf5aae1ac787262929b123c14612.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c309a24d09f7f55b94dd57747d50a3ec",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "67d903f45bce3369f28e",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340d87b35827f2da9c53",
+    "url": "/me/static/js/2.690bef67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.690bef67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d903f45bce3369f28e",
+    "url": "/me/static/js/main.2c30feaf.chunk.js"
+  },
+  {
+    "revision": "62972497a0b6ef7e5116",
+    "url": "/me/static/js/runtime-main.3a9231b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me/static/media/logo.5d5d9eef.svg"
+  }
+]);
