61269136b5f47e9c3aa040234c784c68baceaa0f
kelbek <dev_kelbek@mail.ru>
2020-05-29 22:41:29 +0300

    add font awesome brands and fill contacts page

--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './Contacts.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAt, faDev } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faTelegram, faSkype, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 function Contacts() {
   return (
-    <p> this is Contacts page
-    </p>
+    <div className="ContactsPage">
+      <ul>
+        <li>
+          <a href="https://github.com/kolobok-kelbek" target="_blank">
+            <FontAwesomeIcon icon={faGithub} /> https://github.com/kolobok-kelbek
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/kolobok-kelbek/" target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} /> https://www.linkedin.com/in/kolobok-kelbek/
+          </a>
+        </li>
+        <li>
+          <a href="tg://resolve?domain=kolobok_kelbek" target="_blank">
+            <FontAwesomeIcon icon={faTelegram} /> @kolobok_kelbek
+          </a>
+        </li>
+        <li>
+          <a href="skype:kolobok_kelbek?userinfo" target="_blank">
+            <FontAwesomeIcon icon={faSkype} /> kolobok_kelbek
+          </a>
+        </li>
+        <li>
+          <a href="mailto:dev_kelbek@mail.ru" target="_blank">
+            <FontAwesomeIcon icon={faAt} /> dev_kelbek@mail.ru
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
